0d09f471bdfe5619c09cb9497c6554d55ffb7363
Angor78 <angor78@gmail.com>
2022-11-09 19:43:15 +0300

    Revert "remove test components fix"
    This reverts commit 99ce0f46ef0bc1f3fde1aaf9b37a8b339c0dc750.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
+import {Test1Component} from "./test1/test1.component";
+import { Test2Component } from './test2/test2.component';
+import { Test3Component } from './test3/test3.component';
+import { Test4Component } from './test4/test4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    Test1Component,
+    Test2Component,
+    Test3Component,
+    Test4Component,
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/test3/test3.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'test-test3',
+  template:` <h1 class="title">Go test!</h1>`,
+  styles: [`.title{color:green;}`]
+})
+export class Test3Component {
+
+
+
+}
