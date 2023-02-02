9390526b3c09aab31a5496dde86003fbc9006b20
Angor78 <angor78@gmail.com>
2023-02-02 16:16:54 +0300

    add auth imports

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
   ]
 })
 export class AuthModule { }
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,42 +1,66 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { Injectable } from '@angular/core'
+import { environment } from '../../../environments/environment'
+import { CommonResponse } from '../models/core.models'
+import { ResultCodeEnum } from '../enums/resultCode.enum'
+import { Router } from '@angular/router'
+import { MeResponse } from '../models/auth.models'
+import { catchError, EMPTY } from 'rxjs'
+import {NotificationService} from "./notification.service";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 
-export interface ResponseSignInType {
-  _id: string
-  email: string
-  rememberMe: boolean
-  isAdmin: boolean
-  name: string
-  verified: boolean
-  publicCardPacksCount: number
-  created: string
-  updated: string
-  __v: number
-  token: string
-  tokenDeathTime: number
-  avatar: string
-}
-
-export interface SignInParamsType {
-  email: string
-  password: string
-  rememberMe: boolean
-}
-
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class AuthService {
-
-  constructor(private http: HttpClient) {
-  }
-
   isAuth = false
 
-  // authMe() {
-  //   return this.http
-  //     .post<ResponseSignInType>(`${environment.baseUrl}auth/me`).subscribe(res=>{
-  //       //if(res.resultCode===0)this.isAuth=true
-  //     })
-  // }
+  resolveAuthRequest: Function = () => {}
+  authRequest = new Promise(resolve => {
+    this.resolveAuthRequest = resolve
+  })
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private notificationService: NotificationService
+  ) {}
+
+  login(data: Partial<any>) {
+    this.http
+      .post<CommonResponse<{ userId: number }>>(`${environment.baseUrl}/auth/login`, data)
+      .pipe(catchError(this.errorHandler.bind(this)))
+      .subscribe(res => {
+        if (res.resultCode === 0) {
+          this.router.navigate(['/'])
+        } else {
+          this.notificationService.handleError(res.messages[0])
+        }
+      })
+  }
+
+  logout() {
+    this.http
+      .delete<CommonResponse>(`${environment.baseUrl}/auth/login`)
+      .pipe(catchError(this.errorHandler.bind(this)))
+      .subscribe(res => {
+        if (res.resultCode === ResultCodeEnum.success) {
+          this.router.navigate(['/login'])
+        }
+      })
+  }
+
+  me() {
+    this.http
+      .get<CommonResponse<MeResponse>>(`${environment.baseUrl}/auth/me`)
+      .pipe(catchError(this.errorHandler.bind(this)))
+      .subscribe(res => {
+        if (res.resultCode == ResultCodeEnum.success) {
+          this.isAuth = true
+        }
+        this.resolveAuthRequest()
+      })
+  }
+
+  private errorHandler(err: HttpErrorResponse) {
+    this.notificationService.handleError(err.message)
+    return EMPTY
+  }
 }
