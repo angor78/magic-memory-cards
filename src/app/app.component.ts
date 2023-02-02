c1c593937738f2695381dfae54c50cda9b0d08aa
Angor78 <angor78@gmail.com>
2023-02-02 16:13:47 +0300

    add modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ModalService} from "./shared/services/modal.service";
 
 @Component({
   selector: 'magic-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'magic memory';
+export class AppComponent implements OnInit {
+
+
+  constructor(private modalService: ModalService) {
+  }
+
+  ngOnInit() {
+
+  }
+
+  openModal(id: string) {
+    this.modalService.open(id);
+    console.log('open',id)
+  }
+
+  closeModal(id: string) {
+    this.modalService.close(id);
+  }
 }
