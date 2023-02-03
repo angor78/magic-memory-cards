c889e2fc936f61bfb126cc50be58558a5fc8147a
Angor78 <angor78@gmail.com>
2023-02-03 20:55:15 +0300

    refactor

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { AuthService } from '../../../core/services/auth.service'
 
 @Component({
-  selector: 'app-login',
+  selector: 'magic-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl<string>('safronmanbox@mail.ru', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.pattern('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,5}$'),
+      ],
+    }),
+    password: new FormControl<string>('123456', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(3)],
+    }),
+    rememberMe: new FormControl<boolean>(false, { nonNullable: true }),
+  })
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {
+  get email() {
+    return this.loginForm.get('email')
   }
 
+  get password() {
+    return this.loginForm.get('password')
+  }
+
+  onLoginSubmit() {
+    const value = this.loginForm.value
+    this.authService.login(value)
+  }
 }
